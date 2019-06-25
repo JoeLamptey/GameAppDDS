16c91ccbd168e4211d6eb86b3edb36869dc107a0
JoeLamptey <rutherford.gh@gmail.com>
2019-06-25 17:14:10 +0300

    updated welcome page

--- App.js
+++ App.js
@@ -9,25 +9,42 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import {withAuthenticator} from 'aws-amplify-react-native'
+import {Auth} from 'aws-amplify'
 
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 
 class App extends Component{
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      users: [],
+      current: {email: 'email', phone: 'phone'}
+    }
+  }
+
   componentWillMount(){
-    const userId = "4892a632-7295-4e70-adfb-9bc79bce0f86"
-    this.deleteUser(userId)
-    
+    // const userId = "4892a632-7295-4e70-adfb-9bc79bce0f86"
+    // this.deleteUser(userId)
+    Auth.currentAuthenticatedUser().then((user)=>{
+      let email = user.attributes.email
+      let phone = user.attributes.phone_number
+      //let user = { email, phone } 
+      // console.log(user.attributes.email)
+      // console.log(user.attributes.phone_number)
+      this.setState({current: {email, phone}})
+    }).catch(err=> console.log(err))
   }
 
   listUsers= async()=>{
@@ -43,7 +60,9 @@ class App extends Component{
       }
     `
     await API.graphql(graphqlOperation(listUsers)).then(res=>{
+      users = res.data.listUsers.items
       console.log(res.data.listUsers.items)
+      this.setState({users})
     }).catch(err => console.log('Error: ', err))
   }
 
@@ -83,16 +102,19 @@ class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Distributed System</Text>
+        <Text style={styles.welcome}>Distributed System Game</Text>
         <Text style={styles.instructions}>Innopolis University</Text>
-        <Button title='Users' onPress={this.listUsers}/>
-        {/* <Text style={styles.instructions}>{instructions}</Text> */}
+        <Text style={styles.instructions}>
+          [ {this.state.current.email}, {this.state.current.phone} ]
+        </Text>
+        {/* <Button title='Start Game' onPress={this.listUsers}/> */}
+        <Button title='Start Game' />
       </View>
     );
   }
 }
 
-export default withAuthenticator(App, {includeGreetings: true})
+export default withAuthenticator(App, {includeGreetings: false})
 
 const styles = StyleSheet.create({
   container: {
